c42af03b89916511e7c7f5969c4ad3896f10fde1
Sanilo Alves <saniloalves0@gmail.com>
2023-06-13 10:57:42 -0300

    chore: complete test missing param error signup

--- src/presentation/controllers/user/signup/signup-controller.ts
+++ src/presentation/controllers/user/signup/signup-controller.ts
@@ -4,6 +4,11 @@ import {BadRequest} from '../../../errors/helpers'
 
 export class SignupController implements Controller{
     async action(httpRequest:HttpRequest): Promise<HttpResponse>{
-        return BadRequest(new MissingParamError('name'))
+        const requiredFields = ['name','email','password','confirm_password']
+
+        for(let i = 0; i < requiredFields.length; i++){
+            if(!httpRequest.body[requiredFields[i]]) 
+                return BadRequest(new MissingParamError(requiredFields[i]))
+        }
     }
 }
