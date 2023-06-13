41b698076b7f7fb725e4ad70e72acdea60a721eb
Sanilo Alves <saniloalves0@gmail.com>
2023-06-13 19:20:55 -0300

    change: add user repository

--- src/data/use-cases/user/db-signup.ts
+++ src/data/use-cases/user/db-signup.ts
@@ -1,17 +1,14 @@
 import { UserDTO } from '../../../domain/use-cases/DTOs';
 import {CreateUserUseCase} from '../../../domain/use-cases/user'
 import {Encrypter} from '../../protocols'
+import {UserRepository} from '../../repositories'
 
 export class DbSignup implements CreateUserUseCase{
 
-    constructor(public encrypter:Encrypter){}
+    constructor(public encrypter:Encrypter,public createUserRepository:UserRepository.CreateUser){}
 
     async create(dto: UserDTO.DataEntry.Create){
-        await this.encrypter.encrypter(dto.password)
-        return new Promise<UserDTO.DataOutput.Read>(resolve => resolve({
-            id: 1,
-            name:'name_test',
-            email:'email_test'
-        }))
+        const hashedPassword = await this.encrypter.encrypter(dto.password)
+        return await this.createUserRepository.create(dto)
     }
 }
