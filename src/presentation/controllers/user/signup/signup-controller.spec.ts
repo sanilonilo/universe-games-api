4ca3ccb5555f43ba0cebde0229266306aee74379
Sanilo Alves <saniloalves0@gmail.com>
2023-06-13 10:57:53 -0300

    test: complete test missing param error signup

--- src/presentation/controllers/user/signup/signup-controller.spec.ts
+++ src/presentation/controllers/user/signup/signup-controller.spec.ts
@@ -2,18 +2,67 @@ import {SignupController} from './signup-controller'
 import {MissingParamError} from '../../../errors'
 
 describe('SignupController', () => {
-    test('Missing param name',async () => {
+    test('Missing param name: bad request status 400',async () => {
         const sut = new SignupController()
         const httpRequest = {
             body:{
                 name:'',
                 email:'email@test',
-                password:'password_test'
+                password:'password_test',
+                confirm_password: 'confirm_test'
             }
         }
         const {body,status} = await sut.action(httpRequest)
 
         expect(status).toBe(400)
         expect(body).toEqual(new MissingParamError('name'))
+    })
+
+    test('Missing param email: bad request status 400',async () => {
+        const sut = new SignupController()
+        const httpRequest = {
+            body:{
+                name:'name_test',
+                email:'',
+                password:'password_test',
+                confirm_password: 'confirm_test'
+            }
+        }
+        const {body,status} = await sut.action(httpRequest)
+
+        expect(status).toBe(400)
+        expect(body).toEqual(new MissingParamError('email'))
+    })
+
+    test('Missing param password: bad request status 400',async () => {
+        const sut = new SignupController()
+        const httpRequest = {
+            body:{
+                name:'name_test',
+                email:'email@test',
+                password:'',
+                confirm_password: 'confirm_test'
+            }
+        }
+        const {body,status} = await sut.action(httpRequest)
+
+        expect(status).toBe(400)
+        expect(body).toEqual(new MissingParamError('password'))
+    })
+
+    test('Missing param confirm_password: bad request status 400',async () => {
+        const sut = new SignupController()
+        const httpRequest = {
+            body:{
+                name:'name_test',
+                email:'email@test',
+                password:'password_test',
+                confirm_password: ''
+            }
+        }
+        const {body,status} = await sut.action(httpRequest)
+
+        expect(status).toBe(400)
+        expect(body).toEqual(new MissingParamError('confirm_password'))
     })
 })
