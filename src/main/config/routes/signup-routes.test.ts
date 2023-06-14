d6187cb59bf79c9af0f4021c3195c93ae49b8321
Sanilo Alves <saniloalves0@gmail.com>
2023-06-14 20:35:19 -0300

    test: fix error http request

--- src/main/config/routes/signup-routes.test.ts
+++ src/main/config/routes/signup-routes.test.ts
@@ -1,18 +1,18 @@
-import app from '../app'
-import request from 'supertest'
+import axios from 'axios'
 
 const emaitGenerateFactory = () => `email${Math.random() * 10}@gmail.com`
+const baseURL = 'http://localhost:3220/api'
+const getEndpoint = (url:string) => `${baseURL}/${url}`
 
-describe('Signup route',() => {
+describe('Signup route', () => {
     test('Success signup', async () => {
-        await request(app)
-                .post('/api/signup')
-                .send({
-                    name: 'name_test_integration',
-                    email:emaitGenerateFactory(),
-                    password: 'password_test_integration',
-                    confirm_password: 'password_test_integration'
-                })
-                .expect(200)
+        const { status} = await axios.post(getEndpoint('/signup'), {
+            name: 'name_test_integration',
+            email: emaitGenerateFactory(),
+            password: 'password_test_integration',
+            confirm_password: 'password_test_integration'
+        })
+
+        expect(status).toBe(200)
     })
 })
