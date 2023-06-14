9884b4a1131797980b843fdd164a095ff21ff901
Sanilo Alves <saniloalves0@gmail.com>
2023-06-14 16:36:26 -0300

    chore: import dynamic

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,10 @@
 import express from 'express'
 import setupMiddlewares from './setupMiddlewares'
+import setupRoutes from './setupRoutes'
 
 const app = express()
+
 setupMiddlewares(app)
+setupRoutes(app)
 
 export default app
