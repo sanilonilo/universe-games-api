1f8666a9be4c138cdbd4990cb152b1674c580987
Sanilo Alves <saniloalves0@gmail.com>
2023-06-14 16:57:19 -0300

    fix: complete tests signup controller

--- src/presentation/controllers/user/signup/signup-controller.ts
+++ src/presentation/controllers/user/signup/signup-controller.ts
@@ -1,23 +1,42 @@
-import {Controller,HttpRequest,HttpResponse,EmailValidator} from '../../../protocols'
-import {MissingParamError,InvalidParamError} from '../../../errors'
-import {BadRequest} from '../../../errors/helpers'
+import { Controller, HttpRequest, HttpResponse, EmailValidator } from '../../../protocols'
+import { MissingParamError, InvalidParamError } from '../../../errors'
+import { BadRequest } from '../../../errors/helpers'
+import { CreateUserUseCase } from '../../../../domain/use-cases/user'
 
-export class SignupController implements Controller{
+export class SignupController implements Controller {
 
-    constructor(private readonly emailValidator:EmailValidator){}
+    constructor(
+        private readonly emailValidator: EmailValidator,
+        private readonly createUserUseCase: CreateUserUseCase
+    ) { }
 
-    async action(httpRequest:HttpRequest): Promise<HttpResponse>{
-        const requiredFields = ['name','email','password','confirm_password']
+    async action(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const requiredFields = ['name', 'email', 'password', 'confirm_password']
 
-        for(let i = 0; i < requiredFields.length; i++)
-            if(!httpRequest.body[requiredFields[i]]) 
-                return BadRequest(new MissingParamError(requiredFields[i]))
-        
-        if(httpRequest.body['password'].trim() !== httpRequest.body['confirm_password'].trim())
-            return BadRequest(new Error('Password and password confirmation do not confer'))  
-            
-        const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
+            for (let i = 0; i < requiredFields.length; i++)
+                if (!httpRequest.body[requiredFields[i]])
+                    return BadRequest(new MissingParamError(requiredFields[i]))
 
-        if(!isValidEmail) return BadRequest(new InvalidParamError('email'))
+            if (httpRequest.body['password'].trim() !== httpRequest.body['confirm_password'].trim())
+                return BadRequest(new Error('Password and password confirmation do not confer'))
+
+            const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
+
+            if (!isValidEmail) return BadRequest(new InvalidParamError('email'))
+
+            const response = await this.createUserUseCase.create(httpRequest.body)
+
+            return {
+                body: response,
+                status: 200
+            }
+        }
+        catch (error) {
+            return {
+                body:null,
+                status:500
+            }
+        }
     }
 }
