774ccf04ae65ad672188da364736151e6c23521b
Sanilo Alves <saniloalves0@gmail.com>
2023-06-14 20:06:05 -0300

    test: fix error

--- src/main/config/routes/signup-routes.test.ts
+++ src/main/config/routes/signup-routes.test.ts
@@ -1,13 +1,15 @@
 import app from '../app'
 import request from 'supertest'
 
+const emaitGenerateFactory = () => `email${Math.random() * 10}@gmail.com`
+
 describe('Signup route',() => {
     test('Success signup', async () => {
         await request(app)
                 .post('/api/signup')
                 .send({
                     name: 'name_test_integration',
-                    email:'email@test.integration',
+                    email:emaitGenerateFactory(),
                     password: 'password_test_integration',
                     confirm_password: 'password_test_integration'
                 })
