fdb071de1636f6677bff0b5eb922f4959af69914
Sanilo Alves <saniloalves0@gmail.com>
2023-06-14 20:05:03 -0300

    feat: create express route adapter

---
+++ src/main/config/adapters/express-route-adapter.ts
@@ -0,0 +1,13 @@
+import {Controller,HttpRequest} from '../../../presentation/protocols'
+import {Request,Response} from 'express'
+ 
+export const routeAdapter = (controller:Controller) => {
+    return async (req:Request,res:Response) => {
+        const httpRequest:HttpRequest = {
+            body: req.body
+        }
+        
+        const httpResponse = await controller.action(httpRequest)
+        res.status(httpResponse.status).send(httpResponse.body)
+    }
+}
