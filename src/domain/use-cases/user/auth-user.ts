cf0e8047b3a327d3eb77cb5fa0260174ab50d438
Sanilo Alves <saniloalves0@gmail.com>
2023-06-15 11:14:27 -0300

    refactor: edit use case

--- src/domain/use-cases/user/auth-user.ts
+++ src/domain/use-cases/user/auth-user.ts
@@ -1,5 +1,5 @@
 import {UserDTO} from '../DTOs'
 
 export interface AuthUserUseCase{
-    auth:(dto: UserDTO.DataEntry.Auth) => Promise<UserDTO.DataOutput.Read>
+    auth:(dto: UserDTO.DataEntry.Auth) => Promise<UserDTO.DataOutput.Authenticated>
 }
