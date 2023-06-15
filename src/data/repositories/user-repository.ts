fe96fe2a08273a9cf0a797767b12fbdd5f7c60ff
Sanilo Alves <saniloalves0@gmail.com>
2023-06-15 09:52:26 -0300

    refactor: change user repository

--- src/data/repositories/user-repository.ts
+++ src/data/repositories/user-repository.ts
@@ -4,4 +4,7 @@ export namespace UserRepository{
     export interface CreateUser{
         create:(dto: UserDTO.DataEntry.Create) => Promise<UserDTO.DataOutput.Read>
     }
+    export interface AuthUser{
+        auth:(dto: UserDTO.DataEntry.Auth) => Promise<UserDTO.DataOutput.Read>
+    }
 }
