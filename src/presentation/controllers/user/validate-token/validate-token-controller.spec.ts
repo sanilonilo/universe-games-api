2154db3ce0e0980a7c348e19f22362b8cb52f2af
Sanilo Alves <saniloalves0@gmail.com>
2023-06-15 20:49:10 -0300

    test: validate token controller

--- src/presentation/controllers/user/validate-token/validate-token-controller.spec.ts
+++ src/presentation/controllers/user/validate-token/validate-token-controller.spec.ts
@@ -1,13 +1,6 @@
 import {ValidateTokenController} from './validate-token-controller'
 
 describe('ValidateTokenController', () => {
-    test('Request: throw error', async () => {
-        const sut = new ValidateTokenController()
-        vi.spyOn(sut,'action').mockRejectedValueOnce(new Error())
-
-        await expect(sut.action).rejects.toThrow()
-    })
-
     test('Request: success return status 200', async () => {
         const sut = new ValidateTokenController()
         const httpRequest = {
