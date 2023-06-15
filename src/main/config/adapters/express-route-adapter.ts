8305bc6c380bd74e7b2ec147788d454256da8012
Sanilo Alves <saniloalves0@gmail.com>
2023-06-15 19:57:23 -0300

    refactor: remove lines

--- src/main/config/adapters/express-route-adapter.ts
+++ src/main/config/adapters/express-route-adapter.ts
@@ -3,10 +3,7 @@ import {Request,Response} from 'express'
  
 export const routeAdapter = (controller:Controller) => {
     return async (req:Request,res:Response) => {
-        const httpRequest:HttpRequest = {
-            body: req.body
-        }
-        
+        const httpRequest:HttpRequest = {body: req.body}    
         const httpResponse = await controller.action(httpRequest)
         res.status(httpResponse.status).send(httpResponse.body)
     }
