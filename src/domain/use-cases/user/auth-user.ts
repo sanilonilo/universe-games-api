6a8f40733181d1f21d4c0b167d53dd798c26c915
Sanilo Alves <saniloalves0@gmail.com>
2023-06-15 15:22:21 -0300

    chore: change auth use case

--- src/domain/use-cases/user/auth-user.ts
+++ src/domain/use-cases/user/auth-user.ts
@@ -1,5 +1,5 @@
 import {UserDTO} from '../DTOs'
 
 export interface AuthUserUseCase{
-    auth:(dto: UserDTO.DataEntry.Auth) => Promise<UserDTO.DataOutput.Authenticated>
+    auth:(dto: UserDTO.DataEntry.Auth) => Promise<UserDTO.DataOutput.Authenticated | null>
 }
