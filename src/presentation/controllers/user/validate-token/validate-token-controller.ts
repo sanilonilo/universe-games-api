4312fbdca37c8c6d591898a398a49cd0d918c980
Sanilo Alves <saniloalves0@gmail.com>
2023-06-15 20:49:04 -0300

    refactor: validate token controller

--- src/presentation/controllers/user/validate-token/validate-token-controller.ts
+++ src/presentation/controllers/user/validate-token/validate-token-controller.ts
@@ -3,10 +3,6 @@ import {ServerError} from '../../../errors/helpers'
 
 export class ValidateTokenController implements Controller{
     async action(httpRequest: HttpRequest): Promise<HttpResponse>{
-        try {
-            return {status:201,body:{valid:true}}
-        } catch (error) {
-            return ServerError('Server error')
-        }
+        return {status:201,body:{valid:true}}
     }
 }
