72dcc87f0246dc84b6bcc48e93e0e483a4decf68
Sanilo Alves <saniloalves0@gmail.com>
2023-06-15 16:01:43 -0300

    refactor: complete tests

--- src/presentation/controllers/user/signin/signin-controller.ts
+++ src/presentation/controllers/user/signin/signin-controller.ts
@@ -1,13 +1,25 @@
-import {Controller, HttpRequest, HttpResponse} from '../../../protocols'
-import {BadRequest} from '../../../errors/helpers'
-import {MissingParamError} from '../../../errors'
+import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
+import { BadRequest,ServerError } from '../../../errors/helpers'
+import { MissingParamError } from '../../../errors'
+import { AuthUserUseCase } from '../../../../domain/use-cases/user'
 
-export class SigninController implements Controller{
-    async action(httpRequest: HttpRequest):Promise<HttpResponse>{
-        const requiredFields = ['email','password']
+export class SigninController implements Controller {
 
-        for(let i = 0; i < requiredFields.length; i++)
-            if(!httpRequest.body[requiredFields[i]])
-                return BadRequest(new MissingParamError(requiredFields[i]))
+    constructor(private readonly authUserUseCase: AuthUserUseCase) { }
+
+    async action(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const requiredFields = ['email', 'password']
+
+            for (let i = 0; i < requiredFields.length; i++)
+                if (!httpRequest.body[requiredFields[i]])
+                    return BadRequest(new MissingParamError(requiredFields[i]))
+
+            const payload = await this.authUserUseCase.auth(httpRequest.body)
+
+            return { status: 200, body: payload }
+        } catch (error) {
+            return ServerError('Server error')
+        }
     }
 }
