89c699dec47ca8ec61dc08d0165458c1bcadef3f
Sanilo Alves <saniloalves0@gmail.com>
2023-06-15 16:02:11 -0300

    test: complete tests

--- src/presentation/controllers/user/signin/signin-controller.spec.ts
+++ src/presentation/controllers/user/signin/signin-controller.spec.ts
@@ -1,9 +1,30 @@
 import {MissingParamError} from '../../../errors'
 import {SigninController} from './signin-controller'
+import {AuthUserUseCase} from '../../../../domain/use-cases/user'
+import { UserDTO } from '../../../../domain/use-cases/DTOs'
+
+class AuthUserUseCaseStub implements AuthUserUseCase{
+    async auth(dto: UserDTO.DataEntry.Auth):Promise<UserDTO.DataOutput.Authenticated>{
+        return new Promise(resolve => resolve({
+            name:'name_test',
+            email:'email@test',
+            token:'encoded_token'
+        }))
+    }
+}
+
+const makeSut = () => {
+    const authUserUseCaseStub = new AuthUserUseCaseStub()
+    const sut = new SigninController(authUserUseCaseStub)
+    return {
+        sut,
+        authUserUseCaseStub
+    }
+}
 
 describe('SigninController', () => {
     test('Missing param email: bad request status 400', async () => {
-        const sut = new SigninController()
+        const {sut} = makeSut()
         const httpRequest = {
             body:{
                 email:'',
@@ -18,7 +39,7 @@ describe('SigninController', () => {
     })
 
     test('Missing param password: bad request status 400', async () => {
-        const sut = new SigninController()
+        const {sut} = makeSut()
         const httpRequest = {
             body:{
                 email:'email@test',
@@ -30,5 +51,41 @@ describe('SigninController', () => {
 
         expect(status).toBe(400)
         expect(body).toEqual(new MissingParamError('password'))
+    })
+
+    test('Error server: bad request status 500', async () => {
+        const {sut,authUserUseCaseStub} = makeSut()
+        vi.spyOn(authUserUseCaseStub,'auth').mockReturnValueOnce(new Promise((_,reject) => reject(new Error())))
+        
+        const httpRequest = {
+            body:{
+                email:'email@test',
+                password:'password_test'
+            }
+        }
+
+        const {body,status} = await sut.action(httpRequest)
+        
+        expect(status).toBe(500)
+        expect(body).toBe('Server error')
+    })
+
+    test('User autheticated: success status 200', async () => {
+        const {sut} = makeSut()
+        const httpRequest = {
+            body:{
+                email:'email@test',
+                password:'password_test'
+            }
+        }
+
+        const {body,status} = await sut.action(httpRequest)
+
+        expect(status).toBe(200)
+        expect(body).toEqual({
+            name:'name_test',
+            email:'email@test',
+            token:'encoded_token'
+        })
     })
 })
