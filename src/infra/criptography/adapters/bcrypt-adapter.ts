6688a34a69c48dc40769ef79a938c339f6409705
Sanilo Alves <saniloalves0@gmail.com>
2023-06-15 17:02:10 -0300

    refactor: add compare method

--- src/infra/criptography/adapters/bcrypt-adapter.ts
+++ src/infra/criptography/adapters/bcrypt-adapter.ts
@@ -1,9 +1,13 @@
-import {Encrypter} from '../../../data/protocols'
+import {Encrypter,CompareEncryptHash} from '../../../data/protocols'
 import bcrypt from 'bcrypt'
 
-export class BcryptAdapter implements Encrypter{
+export class BcryptAdapter implements Encrypter,CompareEncryptHash{
     async encrypter(value: string){
         const salt = await bcrypt.genSalt(10)
         return await bcrypt.hash(value,salt)
+    }
+    
+    async compare(value: string, valueHashed: string):Promise<boolean>{
+        return await bcrypt.compare(value,valueHashed)
     }
 }
