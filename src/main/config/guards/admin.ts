441fdb36478423c9f01d4d552f0119bfed403d4c
Sanilo Alves <saniloalves0@gmail.com>
2023-06-16 10:11:12 -0300

    chore: admin guard

--- src/main/config/guards/admin.ts
+++ src/main/config/guards/admin.ts
@@ -3,6 +3,6 @@ import {Request,Response,NextFunction} from 'express'
 export default (middleware:(_req:Request,_res:Response,_next?:NextFunction) => void) => {
     return (req:Request,res:Response) => {
         if(req.user && req.user['admin']) middleware(req,res)
-        else res.status(401).send('Admin')
+        else res.status(401).send()
     }
 }
