53b444651c94020790b27e88c236612849c8c2c4
Sanilo Alves <saniloalves0@gmail.com>
2023-06-16 13:24:06 -0300

    feat: create db create category

---
+++ src/data/use-cases/games/category/db-create-category-game.ts
@@ -0,0 +1,12 @@
+import { GameDTO } from '../../../../domain/use-cases/DTOs';
+import {CreateCategoryGameUseCase} from '../../../../domain/use-cases/games/category'
+import {GameRepository} from '../../../repositories'
+
+export class DbCreateCategoryGame implements CreateCategoryGameUseCase{
+    constructor(public createCategoryRepository:GameRepository.Category.CreateCategory){}
+
+    async create(dto: GameDTO.DataEntry.Category.Create):Promise<GameDTO.DataOutput.Category.Read>{
+        return await this.createCategoryRepository.create(dto)
+    }
+
+}
