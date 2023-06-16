ab9e92069dc577244b3f196982019091805aba55
Sanilo Alves <saniloalves0@gmail.com>
2023-06-16 11:04:45 -0300

    feat: create game dto

---
+++ src/domain/use-cases/DTOs/game-dto.ts
@@ -0,0 +1,31 @@
+export namespace GameDTO{
+    export namespace DataEntry{
+        export namespace Category{
+            export interface Read{
+                id: any
+            }
+    
+            export interface Create{
+                name: string
+            }
+    
+            export interface Update{
+                id: any
+                name: string
+            }
+    
+            export interface Delete{
+                id: any
+            }
+        }
+    }
+
+    export namespace DataOutput{
+        export namespace Category{
+            export interface Read{
+                id: any
+                name: string
+            }
+        }
+    }
+}
