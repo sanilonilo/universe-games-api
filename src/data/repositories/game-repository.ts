449f7255a6285d1c9275ed3b85aa777a80eef0bc
Sanilo Alves <saniloalves0@gmail.com>
2023-06-16 11:31:28 -0300

    feat: create game repository interface

---
+++ src/data/repositories/game-repository.ts
@@ -0,0 +1,21 @@
+import {GameDTO} from '../../domain/use-cases/DTOs'
+
+export namespace GameRepository{
+    export namespace Category{
+        export interface ReadCategory{
+            read:(dto: GameDTO.DataEntry.Category.Read) => Promise<GameDTO.DataOutput.Category.Read>
+        }
+
+        export interface CreateCategory{
+            create:(dto: GameDTO.DataEntry.Category.Create) => Promise<GameDTO.DataOutput.Category.Read>
+        }
+
+        export interface UpdateCategory{
+            update:(dto: GameDTO.DataEntry.Category.Update) => Promise<GameDTO.DataOutput.Category.Read>
+        }
+
+        export interface DeleteCategory{
+            delete:(dto: GameDTO.DataEntry.Category.Delete) => Promise<any>
+        }
+    }
+}
