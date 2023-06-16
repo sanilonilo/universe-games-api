d7cbd3b4d3a3debd4509f15b92203bc4aa38e08f
Sanilo Alves <saniloalves0@gmail.com>
2023-06-16 14:06:44 -0300

    feat: create category postgres respository

---
+++ src/infra/db/postgres/repositories/games/category/create-category-postgres-repository.ts
@@ -0,0 +1,10 @@
+import {GameRepository} from '../../../../../../data/repositories'
+import { GameDTO } from '../../../../../../domain/use-cases/DTOs';
+import {database} from '../../../knex/database'
+
+export class CreateCategoryPostgresRepository implements GameRepository.Category.CreateCategory{
+    async create(dto: GameDTO.DataEntry.Category.Create): Promise<GameDTO.DataOutput.Category.Read>{
+        const categoryCreated = await database.table('game-categories').returning(['id','name']).insert<GameDTO.DataOutput.Category.Read[]>(dto)
+        return categoryCreated[0]
+    }
+}
