be9ad78ab97b9224b0759ae9a5f0a2549383153a
Sanilo Alves <saniloalves0@gmail.com>
2023-06-19 15:14:21 -0300

    feat: create postgres repository read and delete

--- src/infra/db/postgres/repositories/games/category/index.ts
+++ src/infra/db/postgres/repositories/games/category/index.ts
@@ -1,2 +1,4 @@
 export * from './create-category-postgres-repository'
 export * from './update-category-postgres-repository'
+export * from './read-category-postgres-repository'
+export * from './delete-category-postgres-repository'
---
+++ src/infra/db/postgres/repositories/games/category/read-category-postgres-repository.ts
@@ -0,0 +1,10 @@
+import { GameDTO } from '../../../../../../data/DTOs';
+import {GameRepository} from '../../../../../../data/repositories'
+import {database} from '../../../knex/database'
+
+export class ReadCategoryPostgresRespository implements GameRepository.Category.ReadCategory{
+    async read(dto: GameDTO.DataEntry.Category.Read):Promise<GameDTO.DataOutput.Category.Read>{
+        const categoryDB = await database.table('game-categories').select('*').where({id:dto.id}).first<GameDTO.DataOutput.Category.Read>()
+        return categoryDB
+    }
+}
