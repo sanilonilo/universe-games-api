51d3a73b7dd4c8feaf481869a93c12505c8ce7cd
Sanilo Alves <saniloalves0@gmail.com>
2023-06-20 20:27:57 -0300

    refactor: refact game dto

--- src/data/DTOs/game-dto.ts
+++ src/data/DTOs/game-dto.ts
@@ -19,6 +19,35 @@ export namespace GameDTO{
                 id: any
             }
         }
+
+        export namespace Game{
+            export interface Read{
+                id: any
+            }
+
+            export interface ReadAll{
+                page: number
+            }
+    
+            export interface Create{
+                name: string
+                description: string
+                image_url: string
+                trailer_url: string
+            }
+    
+            export interface Update{
+                id: any
+                name: string
+                description: string
+                image_url: string
+                trailer_url: string
+            }
+    
+            export interface Delete{
+                id: any
+            }
+        }
     }
 
     export namespace DataOutput{
@@ -26,6 +55,22 @@ export namespace GameDTO{
             export interface Read{
                 id: any
                 name: string
+            }
+        }
+
+        export namespace Game{
+            export interface Read{
+                id: any
+                name: string
+                description: string
+                image_url: string
+                trailer_url: string
+            }
+
+            export interface ReadAll{
+                page: number
+                total: number | string
+                data: GameDTO.DataOutput.Game.Read[]
             }
         }
     }
