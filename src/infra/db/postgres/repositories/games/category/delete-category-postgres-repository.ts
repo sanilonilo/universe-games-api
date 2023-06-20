2db3f9cac118daa9fbf9c1d132d3f1c258b7ffb1
Sanilo Alves <saniloalves0@gmail.com>
2023-06-20 16:18:16 -0300

    refactor: rename classes

--- src/infra/db/postgres/repositories/games/category/delete-category-postgres-repository.ts
+++ src/infra/db/postgres/repositories/games/category/delete-category-postgres-repository.ts
@@ -2,7 +2,7 @@ import { GameDTO } from '../../../../../../data/DTOs';
 import {GameRepository} from '../../../../../../data/repositories'
 import {database} from '../../../knex/database'
 
-export class DeleteCategoryPostgresRespository implements GameRepository.Category.DeleteCategory{
+export class DeleteCategoryPostgresRepository implements GameRepository.Category.DeleteCategory{
     async delete(dto: GameDTO.DataEntry.Category.Delete):Promise<any>{
         const categoryDB = await database.table('game-categories').where({id:dto.id}).del()
         return categoryDB
--- src/infra/db/postgres/repositories/games/category/update-category-postgres-repository.ts
+++ src/infra/db/postgres/repositories/games/category/update-category-postgres-repository.ts
@@ -2,7 +2,7 @@ import { GameDTO } from '../../../../../../data/DTOs';
 import {GameRepository} from '../../../../../../data/repositories'
 import {database} from '../../../knex/database'
 
-export class UpdateCategoryPostgresRespository implements GameRepository.Category.UpdateCategory{
+export class UpdateCategoryPostgresRepository implements GameRepository.Category.UpdateCategory{
     async update(dto: GameDTO.DataEntry.Category.Update):Promise<GameDTO.DataOutput.Category.Read>{
         const categoryUpdated = await database.table('game-categories').returning(['id','name']).update<GameDTO.DataOutput.Category.Read[]>({name:dto.name}).where({id:dto.id})
         return categoryUpdated[0]
