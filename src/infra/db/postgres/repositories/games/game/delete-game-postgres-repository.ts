fd07f9d213b9527e81cb3aba284c2f43d069bd47
Sanilo Alves <saniloalves0@gmail.com>
2023-06-21 15:24:59 -0300

    feat: add delete game postgres repository

---
+++ src/infra/db/postgres/repositories/games/game/delete-game-postgres-repository.ts
@@ -0,0 +1,9 @@
+import {GameRepository} from '../../../../../../data/repositories'
+import {database} from '../../../knex/database'
+
+export class DeleteGamePostgresRepository implements GameRepository.Game.DeleteGame{
+    async delete(identifier:any): Promise<any>{
+        const game = await database.table('games').where({id:identifier.id}).del()
+        return game
+    }
+}
--- src/infra/db/postgres/repositories/games/game/index.ts
+++ src/infra/db/postgres/repositories/games/game/index.ts
@@ -1,3 +1,4 @@
 export * from './create-game-postgres-repository'
 export * from './update-game-postgres-repository'
 export * from './read-game-postgres-repository'
+export * from './delete-game-postgres-repository'
