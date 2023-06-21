8c4b94777c7576f6224b1259596bf399bae01bd1
Sanilo Alves <saniloalves0@gmail.com>
2023-06-21 10:07:59 -0300

    refactor: change name use case import

--- src/data/use-cases/games/game/db-create-game.ts
+++ src/data/use-cases/games/game/db-create-game.ts
@@ -1,8 +1,8 @@
 import { Game } from '../../../../domain/entities';
-import {CreateGameUsecase} from '../../../../domain/use-cases/games/game'
+import {CreateGameUseCase} from '../../../../domain/use-cases/games/game'
 import {GameRepository} from '../../../repositories'
 
-export class DbCreateGame implements CreateGameUsecase{
+export class DbCreateGame implements CreateGameUseCase{
     constructor(public createGameRepository:GameRepository.Game.CreateGame){}
     async create (dto: Game): Promise<Game>{
         return await this.createGameRepository.create(dto)
