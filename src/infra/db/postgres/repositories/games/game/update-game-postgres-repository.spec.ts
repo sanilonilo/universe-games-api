88f0b6befe58ca1a41562c0cbdb3acbdf856f614
Sanilo Alves <saniloalves0@gmail.com>
2023-06-21 15:04:44 -0300

    test: add update game postgres repository

---
+++ src/infra/db/postgres/repositories/games/game/update-game-postgres-repository.spec.ts
@@ -0,0 +1,34 @@
+import {UpdateGamePostgresRepository} from './'
+
+const makeSut = () => {
+    const sut = new UpdateGamePostgresRepository()
+    return {
+        sut
+    }
+}
+
+describe('UpdateGamePostgresRepository', () => {
+    test('Update game: update data error', async () => {
+        const {sut} = makeSut()
+
+        vi.spyOn(sut,'update').mockRejectedValueOnce(new Error())
+
+        await expect(sut.update).rejects.toThrow()
+    })
+
+    test('Update game: update data success', async () => {
+        const {sut} = makeSut()
+        const data = {
+            id:4,
+            name:'name_updated_test',
+            description:'description_updated_test',
+            image_url:'image_updated_test',
+            trailer_url:'trailer_updated_test',
+            category_id: null
+        }
+
+        const game = await sut.update(data)
+
+        expect(game).toEqual(data)
+    })
+})
