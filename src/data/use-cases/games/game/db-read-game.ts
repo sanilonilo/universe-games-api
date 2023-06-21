18edb1a3b8d86a126db67cc985af06d1fa17f235
Sanilo Alves <saniloalves0@gmail.com>
2023-06-21 10:07:33 -0300

    refactor: change name use cases import

--- src/data/use-cases/games/game/db-read-game.ts
+++ src/data/use-cases/games/game/db-read-game.ts
@@ -1,8 +1,8 @@
 import { Game } from '../../../../domain/entities';
-import {ReadGameUsecase} from '../../../../domain/use-cases/games/game'
+import {ReadGameUseCase} from '../../../../domain/use-cases/games/game'
 import {GameRepository} from '../../../repositories'
 
-export class DbReadGame implements ReadGameUsecase{
+export class DbReadGame implements ReadGameUseCase{
     constructor(public readGameRepository:GameRepository.Game.ReadGame){}
     async read (identifier: any): Promise<Game>{
         return await this.readGameRepository.read(identifier)
--- src/data/use-cases/games/game/db-update-game.ts
+++ src/data/use-cases/games/game/db-update-game.ts
@@ -1,9 +1,9 @@
 import { Game } from '../../../../domain/entities';
-import {UpdateGameUsecase} from '../../../../domain/use-cases/games/game'
+import {UpdateGameUseCase} from '../../../../domain/use-cases/games/game'
 import {GameRepository} from '../../../repositories'
 import {GameDTO} from '../../../DTOs'
 
-export class DbUpdateGame implements UpdateGameUsecase{
+export class DbUpdateGame implements UpdateGameUseCase{
     constructor(public updateGameRepository:GameRepository.Game.UpdateGame){}
     async update (dto: Game): Promise<Game>{
         return await this.updateGameRepository.update(dto as GameDTO.DataEntry.Game.Update)
