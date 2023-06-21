7d19c9f6b16020d4b2b54496454a64da73dd04ad
Sanilo Alves <saniloalves0@gmail.com>
2023-06-21 10:08:46 -0300

    test: create db read all and delete game

---
+++ src/data/use-cases/games/game/db-delete-game.spec.ts
@@ -0,0 +1,39 @@
+import {DbDeleteGame} from './db-delete-game'
+import {GameRepository} from '../../../repositories'
+
+class DeleteGameRepositoryStub implements GameRepository.Game.DeleteGame{
+    async delete(identifier:any): Promise<any>{
+        return new Promise<void>(resolve => resolve())
+    }
+}
+
+const makeSut = () => {
+    const deleteGameRepositoryStub = new DeleteGameRepositoryStub()
+    const sut = new DbDeleteGame(deleteGameRepositoryStub)
+    return {
+        sut,
+        deleteGameRepositoryStub
+    }
+}
+
+describe('DbDeleteGame', () => {
+    test('Delete game repository: throw error', async () => {
+        const {deleteGameRepositoryStub} = makeSut()
+
+        vi.spyOn(deleteGameRepositoryStub,'delete').mockRejectedValueOnce(new Error())
+
+        await expect(deleteGameRepositoryStub.delete).rejects.toThrow()
+    })
+
+    test('Delete game repository: success', async () => {
+        const {sut} = makeSut()
+        vi.spyOn(sut,'delete')
+        const identifier = {
+            id:1
+        }
+        
+        await sut.delete(identifier)
+
+        expect(sut.delete).toBeTruthy()
+    })
+})
---
+++ src/data/use-cases/games/game/db-read-all-game.spec.ts
@@ -0,0 +1,58 @@
+import {DbReadAllGame} from './db-read-all-game'
+import {GameRepository} from '../../../repositories'
+import { GameDTO } from '../../../DTOs'
+
+class ReadAllGameRepositoryStub implements GameRepository.Game.ReadAllGame{
+    async readAll(dto:GameDTO.DataEntry.Game.ReadAll): Promise<GameDTO.DataOutput.Game.ReadAll>{
+        return new Promise(resolve => resolve({
+            page:1,
+            total:10,
+            data:[{
+                id:1,
+                name:'name_test',
+                description:'description_test',
+                image_url:'image_test',
+                trailer_url:'trailer_test'
+            }]
+        }))
+    }
+}
+
+const makeSut = () => {
+    const readAllGameRepositoryStub = new ReadAllGameRepositoryStub()
+    const sut = new DbReadAllGame(readAllGameRepositoryStub)
+    return {
+        sut,
+        readAllGameRepositoryStub
+    }
+}
+
+describe('DbReadAllGame', () => {
+    test('Read all game repository: throw error', async () => {
+        const {readAllGameRepositoryStub} = makeSut()
+
+        vi.spyOn(readAllGameRepositoryStub,'readAll').mockRejectedValueOnce(new Error())
+
+        await expect(readAllGameRepositoryStub.readAll).rejects.toThrow()
+    })
+
+    test('Read game all repository: success', async () => {
+        const {sut} = makeSut()
+        const options = {
+            page:1
+        }
+        const games = await sut.readAll(options)
+
+        expect(games).toEqual({
+            page:options.page,
+            total:10,
+            data:[{
+                id:1,
+                name:'name_test',
+                description:'description_test',
+                image_url:'image_test',
+                trailer_url:'trailer_test'
+            }]
+        })
+    })
+})
