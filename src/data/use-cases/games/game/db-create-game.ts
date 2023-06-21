2d919004b6c67fa68b5f355099bd84c31f00056b
Sanilo Alves <saniloalves0@gmail.com>
2023-06-20 21:06:22 -0300

    refactor: change dependence name in constructor

--- src/data/use-cases/games/game/db-create-game.ts
+++ src/data/use-cases/games/game/db-create-game.ts
@@ -3,8 +3,8 @@ import {CreateGameUsecase} from '../../../../domain/use-cases/games/game'
 import {GameRepository} from '../../../repositories'
 
 export class DbCreateGame implements CreateGameUsecase{
-    constructor(public gameRepository:GameRepository.Game.CreateGame){}
+    constructor(public createGameRepository:GameRepository.Game.CreateGame){}
     async create (dto: Game): Promise<Game>{
-        return await this.gameRepository.create(dto)
+        return await this.createGameRepository.create(dto)
     }
 }
