2c544e1b1d86b9f9037c8b85d87c7b3b233c084e
Sanilo Alves <saniloalves0@gmail.com>
2023-06-21 20:58:58 -0300

    test: fix error postgres repository

--- src/infra/db/postgres/repositories/games/game/read-all-game-postgres-repository.spec.ts
+++ src/infra/db/postgres/repositories/games/game/read-all-game-postgres-repository.spec.ts
@@ -16,6 +16,19 @@ describe('ReadAllGamePostgresRepository', () => {
         await expect(sut.readAll).rejects.toThrow()
     })
 
+    test('Read all game no page param: read all data success', async () => {
+        const {sut} = makeSut()
+        const options = {
+            page: null
+        }
+       
+        const response = await sut.readAll(options)
+
+        expect(Array.isArray(response.data)).toBe(true)
+        expect(response.page).toBe(1)
+        expect(!!response.total || response.total == 0).toBe(true)
+    })
+
     test('Read all game: read all data success', async () => {
         const {sut} = makeSut()
         const options = {
@@ -26,5 +39,6 @@ describe('ReadAllGamePostgresRepository', () => {
 
         expect(Array.isArray(response.data)).toBe(true)
         expect(response.page).toBe(options.page)
+        expect(!!response.total || response.total == 0).toBe(true)
     })
 })
