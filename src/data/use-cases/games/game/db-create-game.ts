a78ca9c85a5ce8fcd34f4a485a5dda87d45db860
Sanilo Alves <saniloalves0@gmail.com>
2023-06-22 10:13:17 -0300

    fix: fix type error

--- src/data/use-cases/games/game/db-create-game.ts
+++ src/data/use-cases/games/game/db-create-game.ts
@@ -1,10 +1,11 @@
 import { Game } from '../../../../domain/entities';
 import {CreateGameUseCase} from '../../../../domain/use-cases/games/game'
 import {GameRepository} from '../../../repositories'
+import {GameDTO} from '../../../DTOs'
 
 export class DbCreateGame implements CreateGameUseCase{
     constructor(public createGameRepository:GameRepository.Game.CreateGame){}
     async create (dto: Game): Promise<Game>{
-        return await this.createGameRepository.create(dto)
+        return await this.createGameRepository.create(dto as GameDTO.DataEntry.Game.Create)
     }
 }
