f7ee7fe1b82e069c998930cf57aeca7e331cd25a
Sanilo Alves <saniloalves0@gmail.com>
2023-06-26 15:42:09 -0300

    chore: set seed user

--- src/infra/db/postgres/knex/seeds/user-seed.ts
+++ src/infra/db/postgres/knex/seeds/user-seed.ts
@@ -5,7 +5,6 @@ export const seed =  (knex:Knex) => {
     return knex('users').del().then(() => {
         return knex('users').insert([
             {
-                id:1,
                 name:'name_test_seed',
                 email:'email@test.seed',
                 password:'password_test_seed',
