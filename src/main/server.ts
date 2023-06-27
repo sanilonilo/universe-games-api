c67da88ae2f228033fc52da65e558d2159ab233b
Sanilo Alves <saniloalves0@gmail.com>
2023-06-27 11:18:57 -0300

    chore: set port

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,5 @@
 import app from './config/app'
 
-const port = process.env.PORT || 3333
+const port = parseInt(process.env.PORT) || 3333
 
-app.listen(port, () => console.log('Server is running in port 3220'))
+app.listen(port, () => console.log(`Server is running in port ${port}`))
